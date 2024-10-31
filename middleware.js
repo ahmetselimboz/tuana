bf2610f1965599bde5e93bba714af38c4302c35d
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2024-10-31 16:48:34 +0300

    00

--- middleware.js
+++ middleware.js
@@ -1,31 +1,42 @@
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers"; 
+import { cookies } from "next/headers";
 
 export function middleware(req) {
-  
-  const cookies = req.headers.get("cookie");
-  const token = cookies
-    ? cookies.split("; ").find((c) => c.startsWith("accessToken="))?.split("=")[1]
-    : null;
+  const cookies = req.headers.get("cookie") || "";
+  const token =
+    cookies
+      .split("; ")
+      .find((c) => c.startsWith("accessToken="))
+      ?.split("=")[1] || null;
 
   console.log("🚀 ~ middleware ~ token:", token);
+  const allCookies = req.cookies.getAll();
+  console.log("🚀 ~ middleware ~ allCookies:", allCookies);
 
-  const protectedRoutes = ["/plans", "/projects", "/add-project", "/redirect", "/analytics"];
-  const unProtectedRoutes = ["/login", "/sign-up", "/email-confirmed", "/forgot-password"];
+  const protectedRoutes = [
+    "/plans",
+    "/projects",
+    "/add-project",
+    "/redirect",
+    "/analytics",
+  ];
+  const unProtectedRoutes = [
+    "/login",
+    "/sign-up",
+    "/email-confirmed",
+    "/forgot-password",
+  ];
   const regularRoutes = ["/"];
-
 
   if (token && regularRoutes.includes(req.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/redirect", req.url));
   }
 
-
   if (req.nextUrl.pathname === "/analytics") {
-   
     if (!req.nextUrl.search) {
       return NextResponse.redirect(new URL("/", req.url));
     }
-    
+
     if (req.nextUrl.search === "?id=TNAKLYTP") {
       return NextResponse.next();
     }
@@ -35,12 +46,10 @@ export function middleware(req) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  
   if (token && unProtectedRoutes.includes(req.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/redirect", req.url));
   }
 
-  
   return NextResponse.next();
 }
 
