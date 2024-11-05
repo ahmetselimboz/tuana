df10c563c780159835c02ebdca254791a6eb364b
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2024-11-05 20:03:49 +0300

    00

--- app/components/Homepage/Grid.jsx
+++ app/components/Homepage/Grid.jsx
@@ -21,6 +21,7 @@ import { LampDemo } from "../Animation/Lamp";
 
 import ThreeDCard from "./ThreeDCard";
 import { FeaturesSectionDemo } from "../Animation/FeaturesSectionDemo";
+import { InfiniteMovingCardsDemo } from "../Animation/InfiniteMovingCards";
 
 export function BentoGridDemo() {
     return (
@@ -61,21 +62,10 @@ export function BentoGridDemo() {
 
                 </div>
             </div>
-            {/* <BentoGrid className="max-w-4xl mx-auto">
-                {items.map((item, i) => (
-
-                    <BentoGridItem
-                        key={i}
-                        index={i}
-                        title={item.title}
-                        description={item.description}
-                        header={item.header}
-                        icon={item.icon}
-                        className={i === 3 || i === 6 ? "md:col-span-2" : ""} />
-
-                ))}
-            </BentoGrid> */}
+            
             <FeaturesSectionDemo></FeaturesSectionDemo>
+
+            <InfiniteMovingCardsDemo></InfiniteMovingCardsDemo>
 
         </div>
     );
