6b81ebcf2d6209d6c856ab8ddeae04d1d796cea2
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2024-11-08 15:06:55 +0300

    00

--- app/components/Add-Project/verifying.jsx
+++ app/components/Add-Project/verifying.jsx
@@ -119,7 +119,7 @@ const Verifying = ({ setStage, appDetails }) => {
                     )
                 }
                 {
-                    error ? (
+                    res2.code !== 200 ? (
                         <div className='text-4xl text-primaryGray mb-4'>
                             Verification Failed!
                         </div>
@@ -150,7 +150,7 @@ const Verifying = ({ setStage, appDetails }) => {
                 }
 
                 {
-                    error ? (
+                    res2.code !== 200 ? (
                         <div>
                             <MdErrorOutline className="text-red-600 text-6xl" />
                         </div>
